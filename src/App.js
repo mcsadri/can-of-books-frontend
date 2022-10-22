91349f0c5b895e143a70f6abde835661cf0607aa
Manuch <60802510+mcsadri@users.noreply.github.com>
2022-10-22 14:09:19 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import BestBooks from './BestBooks';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Header />
+          <Routes>
+            <Route 
+              exact path="/"
+              element={<BestBooks />}
+            >
+            </Route>
+            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+          </Routes>
+          <Footer />
+        </Router>
+      </>
+    )
+  }
+}
+
+export default App;
