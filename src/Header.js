6717fbe414f1c6a0b849dfabf0922c8cc1094055
Andra <94204279+asteele1493@users.noreply.github.com>
2022-10-24 21:49:05 -0700

    Completed lab11

--- src/Header.js
+++ src/Header.js
@@ -5,10 +5,11 @@ import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="secondary" variant="dark" p="10px">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         {/* PLACEHOLDER: render a navigation link to the about page */}
+        <NavItem><Link to="/About" className="nav-link">About</Link></NavItem>
       </Navbar>
     )
   }
