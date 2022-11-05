7ded81e02d9d8275616b7ff05a87e3916d423532
Manuch Sadri <colonquebang@hotmail.com>
2022-11-05 14:52:16 -0700

    troublshooting merge and deploy

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Auth0Provider } from "@auth0/auth0-react";
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Login from './Login';
-import Logout from './Logout';
-import Profile from './Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
